14149caa60cc2e63924593573f38cc04f459063f
Ali Mugamai <113056138+Ali-Mugamai@users.noreply.github.com>
2022-11-22 22:39:27 -0500

    jnj

--- js/script.js
+++ js/script.js
@@ -1,23 +1,42 @@
-// Copyright (c) 2020 ali-mugamai All rights reserved
+// Copyright (c) 2020 Mr. Coxall All rights reserved
 //
-// Created by: ali mugamai
+// Created by: Mr. Coxall
 // Created on: Sep 2020
 // This file contains the JS functions for index.html
-
-"use strict"
 
 /**
  * Check servie worker.
  */
-if (navigator.serviceWorker) {
-  navigator.serviceWorker.register("/ICS2O-PWA-Test/sw.js", {
-    scope: "/ICS2O-PWA-Test/",
-  })
+ if (navigator.serviceWorker) {
+  navigator.serviceWorker.register(
+    "/ICS20-5-02-JS/sw.js",
+    {
+      scope: "/ICS20-5-02-JS/",
+    }
+  )
 }
 
 /**
- * This function displays an alert.
+ * This function updates the slider value.
+ */
+function updateSliderValue(valueFromSlider) {
+  document.getElementById("slider-value").innerHTML = valueFromSlider
+}
+
+/**
+ * This function displays the slider value.
  */
 function myButtonClicked() {
-  document.getElementById("hello-world").innerHTML = "<p>Hello, World!</p>"
+  buttonOnChecked = document.getElementById('positive_number').checked
+  
+  const randomNumber = Math.floor(Math.random() * 6) + 1
+  const negative_random = Math.floor(Math.random() * -5) - 1
+  
+  if (buttonOnChecked == true) {
+    document.getElementById("radio-button-value").innerHTML =
+    "<p>Value is: On" + randomNumber + "</p>"
+  } else {
+    document.getElementById("radio-button-value").innerHTML =
+    "<p>Value is: Off" + negative_random + "</p>"
+  }
 }
